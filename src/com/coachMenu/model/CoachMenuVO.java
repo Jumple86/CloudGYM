52df9c7cb4046ca08bacc7ebd165d7494c929478
Jumple86 <le8796038@gmail.com>
2021-09-12 19:32:32 +0800

    coachMenu model DONE

---
+++ src/com/coachMenu/model/CoachMenuVO.java
@@ -0,0 +1,45 @@
+package com.coachMenu.model;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+public class CoachMenuVO implements Serializable {
+	
+	private Integer menuID;
+	private Integer userID;
+	private String menuName;
+	private Timestamp publishDate;
+	private Integer price;
+	
+	
+	public Integer getMenuID() {
+		return menuID;
+	}
+	public void setMenuID(Integer menuID) {
+		this.menuID = menuID;
+	}
+	public Integer getUserID() {
+		return userID;
+	}
+	public void setUserID(Integer userID) {
+		this.userID = userID;
+	}
+	public String getMenuName() {
+		return menuName;
+	}
+	public void setMenuName(String menuName) {
+		this.menuName = menuName;
+	}
+	public Timestamp getPublishDate() {
+		return publishDate;
+	}
+	public void setPublishDate(Timestamp publishDate) {
+		this.publishDate = publishDate;
+	}
+	public Integer getPrice() {
+		return price;
+	}
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+}
