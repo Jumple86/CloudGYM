469aee4d4fd75eff7f14cd0a2c858e498f4d6430
Jumple86 <le8796038@gmail.com>
2021-10-06 12:19:11 +0800

    新增方法

--- src/com/subscription/model/SubscriptionDAO_interface.java
+++ src/com/subscription/model/SubscriptionDAO_interface.java
@@ -7,5 +7,6 @@ public interface SubscriptionDAO_interface {
 	public void update(SubscriptionVO subscriptionVO);
 	public void delete(Integer subNo);
 	public SubscriptionVO findBySubNo(Integer subNo);
+	public List<SubscriptionVO> findByUserID(Integer userID);
 	public List<SubscriptionVO> findAll();
 }
