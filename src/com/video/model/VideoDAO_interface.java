637eacad76b501ad89b0b59d44afb261e78c57eb
ningwei34 <56311681+ningwei34@users.noreply.github.com>
2021-10-07 15:05:51 +0800

    video model

---
+++ src/com/video/model/VideoDAO_interface.java
@@ -0,0 +1,11 @@
+package com.video.model;
+
+import java.util.List;
+
+public interface VideoDAO_interface {
+	void add(VideoVO videoVO);
+	void update(VideoVO videoVO);
+	void delete(Integer videoID);
+	VideoVO findByPrimaryKey(Integer videoID);
+	List<VideoVO> getAll();
+}
---
+++ src/com/video/model/VideoService.java
@@ -0,0 +1,57 @@
+package com.video.model;
+
+import java.io.InputStream;
+import java.sql.Timestamp;
+import java.util.List;
+
+public class VideoService {
+	private VideoDAO_interface dao;
+	
+	public VideoService() {
+		dao = new VideoJDBCDAO();
+	}
+
+	public VideoVO add(Integer userID, String title, Integer price, String intro, byte[] img, InputStream content, String level, Integer duration, Boolean listed, Integer reportedTimes, Timestamp publishTime) {
+		VideoVO videoVO = new VideoVO();
+		videoVO.setUserID(userID);
+		videoVO.setTitle(title);
+		videoVO.setPrice(price);
+		videoVO.setIntro(intro);
+		videoVO.setImg(img);
+		videoVO.setContent(content);
+		videoVO.setLevel(level);
+		videoVO.setDuration(duration);
+		videoVO.setListed(listed);
+		videoVO.setReportedTimes(reportedTimes);
+		videoVO.setPublishTime(publishTime);
+		dao.add(videoVO);
+		return videoVO;
+	}
+
+	public VideoVO update(String title, Integer price, String intro, byte[] img, InputStream content, String level , Integer videoID) {
+		VideoVO videoVO = new VideoVO();
+		videoVO.setTitle(title);
+		videoVO.setPrice(price);
+		videoVO.setIntro(intro);
+		videoVO.setImg(img);
+		videoVO.setContent(content);
+		videoVO.setLevel(level);
+		videoVO.setVideoID(videoID);
+		dao.update(videoVO);
+		return videoVO;
+	}
+
+	public void delete(Integer videoID) {
+		dao.delete(videoID);
+	}
+
+	public VideoVO findByPrimaryKey(Integer videoID) {
+		return dao.findByPrimaryKey(videoID);
+	}
+
+	public List<VideoVO> getAll() {
+		return dao.getAll();
+	}
+	
+	
+}
