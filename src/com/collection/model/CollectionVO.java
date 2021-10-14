acb53cd4860c09a1eee967903a494b04edeced11
Betina Chen <angel59272624@gmail.com>
2021-10-15 02:01:36 +0800

    修改收藏區Collection(table欄位新增itemID,刪除欄位menuID與videoID)

--- src/com/collection/model/CollectionVO.java
+++ src/com/collection/model/CollectionVO.java
@@ -2,50 +2,45 @@ package com.collection.model;
 
 import java.io.Serializable;
 
-public class CollectionVO implements Serializable{
-	
+public class CollectionVO implements Serializable {
+
 	private Integer collectionNo;
 	private Integer userID;
-	private Integer menuID;
-	private Integer videoID;
-		
+	private Integer itemID;
+
 	public CollectionVO() {
 		super();
 	}
-		
-	public CollectionVO(Integer collectionNo, Integer userID, Integer menuID, Integer videoID) {
+
+	public CollectionVO(Integer collectionNo, Integer userID, Integer itemID) {
 		super();
 		this.collectionNo = collectionNo;
 		this.userID = userID;
-		this.menuID = menuID;
-		this.videoID = videoID;
+		this.itemID = itemID;
 	}
 
 	public Integer getCollectionNo() {
 		return collectionNo;
 	}
+
 	public void setCollectionNo(Integer collectionNo) {
 		this.collectionNo = collectionNo;
 	}
+
 	public Integer getUserID() {
 		return userID;
 	}
+
 	public void setUserID(Integer userID) {
 		this.userID = userID;
 	}
-	public Integer getMenuID() {
-		return menuID;
+
+	public Integer getItemID() {
+		return itemID;
 	}
-	public void setMenuID(Integer menuID) {
-		this.menuID = menuID;
+
+	public void setItemID(Integer itemID) {
+		this.itemID = itemID;
 	}
-	public Integer getVideoID() {
-		return videoID;
-	}
-	public void setVideoID(Integer videoID) {
-		this.videoID = videoID;
-	}
-	
-	
 
 }
