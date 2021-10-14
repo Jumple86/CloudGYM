53e40f55086f7c9cd62e5396ccc17c70f86ad6cf
DeliaMo <tingyen984@gmail.com>
2021-10-14 21:51:36 +0800

    修改Admin model

--- src/com/admin/model/AdminJDBCDAO.java
+++ src/com/admin/model/AdminJDBCDAO.java
@@ -11,7 +11,7 @@ public class AdminJDBCDAO implements AdminDAO_interface {
 	public static final String USER = "David";
 	public static final String PASSWORD = "123456";
 
-	private static final String UPDATE_STMT = "UPDATE admin SET adminName = ?, commentAuth = ?, videoAuth = ?, subAuth = ?, userAuth = ? WHERE adminID = ?";
+	private static final String UPDATE_STMT = "UPDATE admin SET adminName = ?,adminPW = ?,commentAuth = ?, videoAuth = ?, subAuth = ?, userAuth = ? WHERE adminID = ?";
 	private static final String FIND_BY_ADMINID_STMT = "SELECT * FROM admin WHERE adminID = ?";
 	private static final String FIND_ALL = "SELECT * FROM admin"; 
 	
@@ -21,7 +21,7 @@ public class AdminJDBCDAO implements AdminDAO_interface {
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
-	}
+	}	
 	
 	@Override
 	public void update(AdminVO adminVO) {
@@ -33,11 +33,12 @@ public class AdminJDBCDAO implements AdminDAO_interface {
 			pstmt = con.prepareStatement(UPDATE_STMT);
 			
 			pstmt.setString(1, adminVO.getAdminName());
-			pstmt.setInt(2, adminVO.getCommentAuth());
-			pstmt.setInt(3, adminVO.getVideoAuth());
-			pstmt.setInt(4, adminVO.getSubAuth());
-			pstmt.setInt(5, adminVO.getUserAuth());
-			pstmt.setInt(6, adminVO.getAdminID());
+			pstmt.setString(2, adminVO.getAdminPW());
+			pstmt.setInt(3, adminVO.getCommentAuth());
+			pstmt.setInt(4, adminVO.getVideoAuth());
+			pstmt.setInt(5, adminVO.getSubAuth());
+			pstmt.setInt(6, adminVO.getUserAuth());
+			pstmt.setInt(7, adminVO.getAdminID());
 			
 			int rowCount = pstmt.executeUpdate();
 			System.out.println("修改" + rowCount + "筆資料");
@@ -62,6 +63,7 @@ public class AdminJDBCDAO implements AdminDAO_interface {
 		}
 
 	}
+	
 
 	@Override
 	public AdminVO findbyAdminID(Integer adminID) {
@@ -81,6 +83,7 @@ public class AdminJDBCDAO implements AdminDAO_interface {
 				adminVO = new AdminVO();
 				adminVO.setAdminID(rs.getInt("adminID"));
 				adminVO.setAdminName(rs.getString("adminName"));
+				adminVO.setAdminPW(rs.getString("adminPW"));
 				adminVO.setCommentAuth(rs.getInt("commentAuth"));
 				adminVO.setVideoAuth(rs.getInt("videoAuth"));
 				adminVO.setSubAuth(rs.getInt("subAuth"));
@@ -136,6 +139,7 @@ public class AdminJDBCDAO implements AdminDAO_interface {
 				adminVO = new AdminVO();
 				adminVO.setAdminID(rs.getInt("adminID"));
 				adminVO.setAdminName(rs.getString("adminName"));
+				adminVO.setAdminPW(rs.getString("adminPW"));
 				adminVO.setCommentAuth(rs.getInt("commentAuth"));
 				adminVO.setVideoAuth(rs.getInt("videoAuth"));
 				adminVO.setSubAuth(rs.getInt("subAuth"));
@@ -177,32 +181,45 @@ public class AdminJDBCDAO implements AdminDAO_interface {
 	public static void main(String[] args) {
 		AdminJDBCDAO dao =new AdminJDBCDAO();
 		
-		//修改
-		AdminVO adminVO1 = new AdminVO();
-		adminVO1.setAdminID(9003);
-		adminVO1.setAdminName("Eva");
-		adminVO1.setCommentAuth(1);
-		adminVO1.setVideoAuth(0);
-		adminVO1.setSubAuth(0);
-		adminVO1.setUserAuth(0);
-		dao.update(adminVO1);
+		//修改全部資料
+//		AdminVO adminVO = new AdminVO();
+//		adminVO.setAdminID(9003);
+//		adminVO.setAdminName("Eva");
+//		adminVO.setAdminPW("777");
+//		adminVO.setCommentAuth(1);
+//		adminVO.setVideoAuth(0);
+//		adminVO.setSubAuth(1);
+//		adminVO.setUserAuth(0);
+//		dao.update(adminVO);
 		
+		//修改單筆資料
+		AdminVO adminVO1 = dao.findbyAdminID(9003);
+//		adminVO1.setAdminID(9003);
+//		adminVO1.setAdminName("Eva");
+//		adminVO1.setAdminPW("797");
+//		adminVO1.setCommentAuth(1);
+//		adminVO1.setVideoAuth(0);
+		adminVO1.setSubAuth(0);
+//		adminVO1.setUserAuth(0);
+		dao.update(adminVO1);
 		
 		//單筆查詢
 		AdminVO adminVO2 = dao.findbyAdminID(9003);
 		System.out.print(adminVO2.getAdminID()+",");
 		System.out.print(adminVO2.getAdminName()+",");
+		System.out.print(adminVO2.getAdminPW()+",");		
 		System.out.print(adminVO2.getCommentAuth()+",");
 		System.out.print(adminVO2.getVideoAuth()+",");
 		System.out.print(adminVO2.getSubAuth()+",");
 		System.out.println(adminVO2.getUserAuth());
 		System.out.println("=================");
 		
-		//查詢
+		//全部查詢
 		List<AdminVO> list = dao.findAll();
 		for (AdminVO avo : list) {
 			System.out.print(avo.getAdminID()+",");
 			System.out.print(avo.getAdminName()+",");
+			System.out.print(avo.getAdminPW()+",");
 			System.out.print(avo.getCommentAuth()+",");
 			System.out.print(avo.getVideoAuth()+",");
 			System.out.print(avo.getSubAuth()+",");
